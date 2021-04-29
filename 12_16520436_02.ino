e98b9aad5b7da627d6156cd834feae1a9a6e97eb
Ilsarstn <82457051+Ilsarstn@users.noreply.github.com>
2021-04-29 15:16:43 +0700

    Create 12_16520436_02.ino

---
+++ 12_16520436_02.ino
@@ -0,0 +1,21 @@
+#inolude <servo.h>
+Servo servo _0 ;
+const int servoPin = 7;
+const int ledPin = 2;
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT) ;
+  servo_0, attach (servoPin) ;
+  servo_0, write(pos) ;
+}
+
+void loop()
+{
+  digitalWritte(2,HIGH);
+  for (pos = 0; pos ,= 90 ; pos += 1)
+      servo_0.write(pos) ;
+      delay(15);
+}
+   delay(2000)
+  }				
